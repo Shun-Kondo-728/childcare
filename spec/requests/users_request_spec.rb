f9a788bd1c11610749c7630c899b1ac30c7b39d3
Shun-Kondo-728 <shun728kon@gmail.com>
2021-05-07 02:58:30 +0900

    ユーザー登録テスト・日本語化対応

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe "Users", type: :request do
     expect(response).to be_success
     expect(response).to have_http_status "200"
   end
+
+  it "有効なユーザーで登録" do
+    expect {
+      post users_path, params: { user: { name: "BabyFood",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    }.to change(User, :count).by(1)
+    redirect_to @user
+    follow_redirect!
+  end
+
+  it "無効なユーザーで登録" do
+    expect {
+      post users_path, params: { user: { name: "BabyFood",
+                                         password: "password",
+                                         password_confirmation: "pass" } }
+    }.not_to change(User, :count)
+  end
 end
