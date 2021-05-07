7fdf3a4b75331df35416b25edd9d0a6c475359f5
Shun-Kondo-728 <shun728kon@gmail.com>
2021-05-07 16:23:15 +0900

    名前の一意性を強制・テスト追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_164257) do
+ActiveRecord::Schema.define(version: 2021_05_07_070957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_164257) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
