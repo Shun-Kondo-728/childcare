2de6ae852328a73976445b55d2fa2672a456bc63
Shun-Kondo-728 <shun728kon@gmail.com>
2021-05-08 02:02:54 +0900

    ログイン・ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = '名前とパスワードの組み合わせが誤っています'
+      render 'new'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
