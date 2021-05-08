621ef18b4d82a8a5138da38dbcb518175cd038cf
Shun-Kondo-728 <shun728kon@gmail.com>
2021-05-09 03:40:26 +0900

    離乳食メモフォーム作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,11 @@
 class MemosController < ApplicationController
+    before_action :logged_in_user
+ 
     def index
         @feed_items = current_user.feed.page(params[:page]).per(5)
+    end
+
+    def new
+        @memo = Memo.new
     end
 end
